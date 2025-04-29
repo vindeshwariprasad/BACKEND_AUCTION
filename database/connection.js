361d5f6d0330e54ffdeddfe3acc359c915e1706b
Vindeshwari Prasad <vinayguptasia53@gmail.com>
2025-04-30 02:12:39 +0530

    Add files via upload

---
+++ database/connection.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+export const connection = () => {
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      dbName: "MERN_AUCTION_PLATFORM",
+    })
+    .then(() => {
+      console.log("Connected to database.");
+    })
+    .catch((err) => {
+      console.log(`Some error occured while connecting to database: ${err}`);
+    });
+};
+
+
+// SsyYnUE9S4c9v3FU
+// vinayguptasia233
